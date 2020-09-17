8f607856553e86fd4ced529069496f8f69dcf914
Felippe <felippe.chemello@kinghost.com.br>
2020-09-17 15:51:28 -0300

    Lambda running locally

--- __video-maker-lambda/index.js
+++ __video-maker-lambda/index.js
@@ -1,7 +1,9 @@
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
+    const body = JSON.parse(event.body)
+    
     const response = {
         statusCode: 200,
-        body: JSON.stringify(`Não acredito que o deploy é tão fácil!`),
+        body: JSON.stringify(`Hello ${body.name}!`),
     };
     return response;
 };
