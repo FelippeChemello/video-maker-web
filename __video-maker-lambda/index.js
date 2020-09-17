81762a7363ae2d62d026ce4fb2fc2ba3accdbba6
Felippe <felippe.chemello@kinghost.com.br>
2020-09-17 14:48:54 -0300

    I don't believe that it works

--- __video-maker-lambda/index.js
+++ __video-maker-lambda/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify(`Hello World from Github Deploy!`),
+        body: JSON.stringify(`Não acredito que o deploy é tão fácil!`),
     };
     return response;
 };
