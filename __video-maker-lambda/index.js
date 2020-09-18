0574383a44ef0aae188afb2aea3d7a1f71973784
Felippe <felippe.chemello@kinghost.com.br>
2020-09-18 12:17:16 -0300

    FFMPEG via exodus

--- __video-maker-lambda/index.js
+++ __video-maker-lambda/index.js
@@ -1,87 +1,87 @@
-// dependencies
-const AWS = require('aws-sdk');
-const util = require('util');
-const sharp = require('sharp');
+const childProcess = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
-// get reference to S3 client
+const AWS = require('aws-sdk');
+const request = require('request');
+const tempy = require('tempy');
+
 const s3 = new AWS.S3();
 
-exports.handler = async (event, context, callback) => {
-    // Read options from the event parameter.
-    console.log('Reading options from event:\n', util.inspect(event, {
-        depth: 5,
-    }));
-    const srcBucket = event.Records[0].s3.bucket.name;
-    // Object key may have spaces or unicode non-ASCII characters.
-    const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    const dstBucket = `${srcBucket}`;
-    const dstKey = `resized-${srcKey}`;
+exports.handler = (event, context, callback) => {
+    // We're going to do the transcoding asynchronously, so we callback immediately.
+    callback();
 
-    console.log(`srcKey: ${srcKey}`);
-    console.log(`srcBucket: ${srcBucket}`);
+    // Extract the event parameters.
+    const {
+        mp3Key,
+        url,
+    } = event;
+    const filename = event.filename || path.basename(mp3Key);
+    const logKey = event.logKey || `${mp3Key}.log`;
+    const s3Bucket = event.s3Bucket || 'youtube-mp3-downloader';
 
-    // Infer the image type from the file suffix.
-    const typeMatch = srcKey.match(/\.([^.]*)$/);
-    if (!typeMatch) {
-        console.log('Could not determine the image type.');
-        return;
-    }
+    // Create temporary input/output filenames that we can clean up afterwards.
+    const inputFilename = tempy.file();
+    const mp3Filename = tempy.file({
+        extension: 'mp3',
+    });
 
-    // Check that the image type is supported
-    const imageType = typeMatch[1].toLowerCase();
-    if (imageType != 'jpg' && imageType != 'png') {
-        console.log(`Unsupported image type: ${imageType}`);
-        return;
-    }
+    // Download the source file.
+    Promise.resolve().then(() => new Promise((resolve, revoke) => {
+        const writeStream = fs.createWriteStream(inputFilename);
+        writeStream.on('finish', resolve);
+        writeStream.on('error', revoke);
+        request(url).pipe(writeStream);
+    }))
+        // Perform the actual transcoding.
+        .then(() => {
+            // Use the Exodus ffmpeg bundled executable.
+            const ffmpeg = path.resolve(__dirname, 'exodus', 'bin', 'ffmpeg');
 
-    // Download the image from the S3 source bucket.
-    let origimage;
-    try {
-        const params = {
-            Bucket: srcBucket,
-            Key: srcKey,
-        };
-        console.log('Buscando imagem no S3');
-        origimage = await s3.getObject(params).promise();
-        console.log('Imagem Original: ');
-        console.log(origimage);
-    } catch (error) {
-        console.log(error);
-        return;
-    }
-
-    // set thumbnail width. Resize will set the height automatically to maintain aspect ratio.
-    const width = 200;
-
-    // Use the Sharp module to resize the image and save in a buffer.
-    let buffer;
-    try {
-        console.log('Iniciando Resize');
-        buffer = await sharp(origimage.Body).resize(width).toBuffer();
-        console.log('Resize completo');
-        console.log(buffer);
-    } catch (error) {
-        console.log(error);
-        return;
-    }
-
-    // Upload the thumbnail image to the destination bucket
-    try {
-        const destparams = {
-            Bucket: dstBucket,
-            Key: dstKey,
-            Body: buffer,
-            ContentType: 'image',
-        };
-        console.log('Salvando Imagem');
-        const putResult = await s3.putObject(destparams).promise();
-        console.log('Imagem salva');
-        console.log(putResult);
-    } catch (error) {
-        console.log(error);
-        return;
-    }
-
-    console.log(`Successfully resized ${srcBucket}/${srcKey
-    } and uploaded to ${dstBucket}/${dstKey}`);
+            // Convert the FLV file to an MP3 file using ffmpeg.
+            const ffmpegArgs = [
+                '-i', inputFilename,
+                '-vn', // Disable the video stream in the output.
+                '-acodec', 'libmp3lame', // Use Lame for the mp3 encoding.
+                '-ac', '2', // Set 2 audio channels.
+                '-q:a', '6', // Set the quality to be roughly 128 kb/s.
+                mp3Filename,
+            ];
+            const process = childProcess.spawnSync(ffmpeg, ffmpegArgs);
+            return process.stdout.toString() + process.stderr.toString();
+        })
+        // Upload the generated MP3 to S3.
+        .then((logContent) => new Promise((resolve, revoke) => {
+            s3.putObject({
+                Body: fs.createReadStream(mp3Filename),
+                Bucket: s3Bucket,
+                Key: mp3Key,
+                ContentDisposition: `attachment; filename="${filename.replace('"', '\'')}"`,
+                ContentType: 'audio/mpeg',
+            }, (error) => {
+                if (error) {
+                    revoke(error);
+                } else {
+                    // Update a log of the FFmpeg output.
+                    const logFilename = path.basename(logKey);
+                    s3.putObject({
+                        Body: logContent,
+                        Bucket: s3Bucket,
+                        ContentType: 'text/plain',
+                        ContentDisposition: `inline; filename="${logFilename.replace('"', '\'')}"`,
+                        Key: logKey,
+                    }, resolve);
+                }
+            });
+        }))
+        .catch(console.error)
+        // Delete the temporary files.
+        .then(() => {
+            [inputFilename, mp3Filename].forEach((filename) => {
+                if (fs.existsSync(filename)) {
+                    fs.unlinkSync(filename);
+                }
+            });
+        });
 };
