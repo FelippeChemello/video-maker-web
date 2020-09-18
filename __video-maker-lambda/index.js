32e43bb4f5f3c399e501174e3301cf02cee28ba9
Felippe <felippe.chemello@kinghost.com.br>
2020-09-17 22:32:09 -0300

    Corrigindo para salvar no mesmo bucket

--- __video-maker-lambda/index.js
+++ __video-maker-lambda/index.js
@@ -14,7 +14,7 @@ exports.handler = async (event, context, callback) => {
     const srcBucket = event.Records[0].s3.bucket.name;
     // Object key may have spaces or unicode non-ASCII characters.
     const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    const dstBucket = `${srcBucket}-resized`;
+    const dstBucket = `${srcBucket}`;
     const dstKey = `resized-${srcKey}`;
 
     // Infer the image type from the file suffix.
